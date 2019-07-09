6f7ee9dc49b11838b9d8eafed942f953075d4a0f
Yueqi Wang <yueqi.wang.pku@gmail.com>
2019-07-08 23:16:42 -0400

    update config

--- ncpsort/config/train_config.py
+++ ncpsort/config/train_config.py
@@ -1,33 +1,32 @@
 
 params = {
-    'model': 'SpikeTemplate',
+    'model': 'SpikeTemplate',  # model name
     'cluster_generator': "MFM",  # or CRP
     'maxK': 12,  # max number of clusters to generate 
 
-    # CRP
-    'crp_alpha': .7,       # dispersion parameter of the Chinese Restaurant Process
-
     # MFM
-    "poisson_lambda": 3 - 1,   # K ~ Pk(k) = Poisson(lambda) + 1
-    "dirichlet_alpha": 1,
+    "poisson_lambda": 3 - 1,  # K ~ Pk(k) = Poisson(lambda) + 1
+    "dirichlet_alpha": 1,  # prior for cluster proportions
 
-    'n_timesteps': 32,
-    'n_channels': 7,
+    # CRP
+    'crp_alpha': .7,  # dispersion parameter of CRP 
 
-    # ResNet encoder: spike_encoder.py params;
+    # data shape 
+    'n_timesteps': 32,  # width of each spike
+    'n_channels': 7,  # number of local channels/units
+
+    # ResNet encoder: parameters for spike_encoder.py 
     'resnet_blocks': [1,1,1,1],
     'resnet_planes': 32,
 
-    # N ~ unif(Nmin, Nmax); total number of data points;
-    # at training time up to 500 usually; testing can do up to 2000
+    # number of data points for training, N ~ unif(Nmin, Nmax)
     'Nmin': 200,
-    'Nmax': 500, #500
+    'Nmax': 500, 
 
-    # NCP layer dimensions; NN architecture; ok to leave fixed for now;
+    # neural net architecture for NCP
     'h_dim': 256,
     'g_dim': 512,
     'H_dim': 128,
-
 }
 
 
